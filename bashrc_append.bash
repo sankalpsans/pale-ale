9e16458c3dc7ea5947b34470db3f67098c18dea8
Sankalp <sankalp@Apples-MacBook-Pro.local>
2016-04-01 08:51:46 +0530

    first commit

---
+++ bashrc_append.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BUCKET=logs.sportskeeda.com
+LOGPATH=loadbalancer_logs/AWSLogs/246435084856/elasticloadbalancing/ap-southeast-1
+YEAR=$(date -d "$y" '+%Y')
+MONTH=$(date -d "$m" '+%m')
+DAY=$(date -d "$d" '+%d')
+
+# Lists log files present for today's date at the relevant location in S3
+function showlogs() {
+ echo Listing Logs from $BUCKET/$LOGPATH/$YEAR/$MONTH/$DAY/;
+ aws s3 ls s3://$BUCKET/$LOGPATH/$YEAR/$MONTH/$DAY/ | sort -n
+}
+
+# accepts filename present in remote directory
+function getlog() {
+ aws s3 cp s3://$BUCKET/$LOGPATH/$YEAR/$MONTH/$DAY/$1 `pwd`
+}
+
+# accepts filename
+function find4xx() {
+ cat $1 | awk {'print $1" "$6" "$7" "$8" "$12" "$13" "$15" "$16" "$17" "$18" "$19'} | grep " 4[0-9][0-9] "
+}
+
+# accepts filename
+function find5xx() {
+ cat $1 | awk {'print $1" "$6" "$7" "$8" "$12" "$13" "$15" "$16" "$17" "$18" "$19'} | grep " 5[0-9][0-9] "
+}
+
+# accepts filename
+function findslow() {
+ cat $1 | awk {'print $6" "$1" "$12" "$13" "$15" "$16" "$17" "$18" "$19" "$20" "$21'} |sort -n | tail -20
+}
