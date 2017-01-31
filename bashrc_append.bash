d32765ff8b3a524530eadae8f6fb52c6b177ce9c
Sankalp <sankalp@sportskeeda.com>
2017-01-31 12:55:52 +0530

    Add function to get slow logs for multiple log files, to see a bigger picture. Currently it only returns the data for 20 most recent access log files

--- bashrc_append.bash
+++ bashrc_append.bash
@@ -31,3 +31,23 @@ function find5xx() {
 function findslow() {
  cat $1 | awk {'print $6" "$1" "$12" "$13" "$15" "$16" "$17" "$18" "$19" "$20" "$21'} |sort -n | tail -20
 }
+
+
+# Shows slow log for the most recent 20 log files. TODO: update this function to accept a generic number of files
+function analyze_slow_logs() {
+ NUMBER_OF_FILES_TO_ANALYZE=20
+ echo Analyzing $NUMBER_OF_FILES_TO_ANALYZE file\(s\)
+ showlogs | tail -$NUMBER_OF_FILES_TO_ANALYZE | awk {'print $4'} > .pale_ale_analysis
+ cat .pale_ale_analysis
+ COUNTER=1
+ while [ $COUNTER -lt $NUMBER_OF_FILES_TO_ANALYZE ]; do
+  echo $COUNTER;
+  LOGFILE_NAME=`tail -$COUNTER .pale_ale_analysis | head -1`
+  echo $LOGFILE_NAME
+  echo Getting logfile $LOGFILE_NAME...
+  getlog $LOGFILE_NAME
+  findslow $LOGFILE_NAME >> .pale_ale_slow
+  let COUNTER=COUNTER+1
+ done
+cat .pale_ale_slow | sort -n
+}
